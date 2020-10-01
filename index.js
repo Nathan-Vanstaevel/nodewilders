4aa293a5f4f7a20bbfa83784f70003e572871339
Nathan-Vanstaevel <nathanvanstaevel@gmail.com>
2020-10-01 10:21:20 +0200

    wilders name files

---
+++ index.js
@@ -0,0 +1,25 @@
+// const wilderName {
+//   name1 : 'Bob',
+//   name2 : 'Bobette',
+//   name3 : 'Quick',
+//   name4 : 'Flukpe'
+// }
+
+// const chalk = require('chalk');
+
+// console.log(chalk.blue(${wilderName.name1})chalk.red(${wilderName.name2})chalk.green(${wilderName.name3})chalk.yellow${wilderName.name4});
+
+let name1 = 'Bob';
+let name2 = 'Bobette';
+let name3 = 'Quick';
+let name4 = 'Flukpe';
+
+// console.log(name1, name2, name3, name4);
+const chalk = require('chalk');
+
+console.log(
+  chalk.blue(name1),
+  chalk.red(name2),
+  chalk.green(name3),
+  chalk.yellow(name4)
+);
